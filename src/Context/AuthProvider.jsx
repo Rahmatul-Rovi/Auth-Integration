c04520b7e9625ad4bd024bd66d81e58562d67f70
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2025-12-09 11:02:12 +0600

    context api added

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../Firebase.init';
 const AuthProvider = ({children}) => {
 
     const createUser = (email, passowrd) => {
         return createUserWithEmailAndPassword(auth, email, passowrd);
     }
+    const signInUser = (email, passowrd) => {
+        return signInWithEmailAndPassword(auth, email, passowrd);
+    }
+
+
+    onAuthStateChanged(auth, (currentUser) => {
+      if(currentUser){
+        console.log('Has current user:', currentUser);
+      }
+      else{
+        console.log(currentUser);
+      }
+    })
     const userInfo = {
-        createUser 
+        createUser,
+        signInUser 
     }
     return (
        <AuthContext value={userInfo}>
